fdb5b33dc1be2472e5cfe9067b5219ccbc4ec5f3
Al Amin Ahamed <mrabir.ahamed@gmail.com>
2021-09-23 16:58:21 +0600

    Update Clock.js

--- src/Components/Clock.js
+++ src/Components/Clock.js
@@ -17,12 +17,17 @@ class Clock extends Component {
         this.setState({ date : new Date() });
     }
 
+    handleClick(){
+        console.log('Button was clicked!!');
+    }
+
     render(){
         return (
             <h1 className='container'>
                 <span className='text'>
                     {this.state.date.toLocaleTimeString(this.props.locale)}
                 </span>
+                <button type='button' onClick={this.handleClick}>CLick Here</button>
             </h1>
         );
     }
