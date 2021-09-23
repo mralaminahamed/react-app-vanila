ddae66bce1aefbb3d086319569f855999cca7788
Al Amin Ahamed <mrabir.ahamed@gmail.com>
2021-09-23 15:59:14 +0600

    Update Images.js

--- src/Components/Images.js
+++ src/Components/Images.js
@@ -1,3 +1,23 @@
+import { Component } from 'react';
 import logo from '../logo.svg';
+import sample01 from '../images/sample-01.jpg';
+import sample02 from '../images/sample-02.jpg';
+import sample03 from '../images/sample-03.jpg';
+
 
 console.log(logo);
+
+class Images extends Component {
+    render(){
+        return (
+            [
+            <img src={logo}/>,
+            <img src={sample01}/>,
+            <img src={sample02}/>,
+            <img src={sample03}/>
+        ]
+        );
+    }
+}
+
+export default Images;
