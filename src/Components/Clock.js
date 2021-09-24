a09cbf71532700727a780ab67eeb73ad20e0c4d5
আল আমিন আহম্মেদ <mrabir.ahamed@gmail.com>
2021-09-25 00:47:05 +0600

    Update Clock.js

--- src/Components/Clock.js
+++ src/Components/Clock.js
@@ -1,8 +1,14 @@
 import { Component } from "react";
+import Button from "./Button";
 
 class Clock extends Component {
-    
-    state = { date : new Date() , locale : 'bn-BD'};
+
+    constructor(props) {
+        super(props);
+        const {locale} = this.props;
+        this.state = { date : new Date() , locale};
+    }
+
 
     componentDidMount(){
         this.clockTimer = setInterval(()=>{
@@ -24,18 +30,19 @@ class Clock extends Component {
 
     render(){
         const {date, locale} = this.state;
-        const containerAll = document.querySelectorAll('img');
+        //const {locale:NextLocale} = this.props;
         return (
-            <h1 className='container'>
+            <h1 className='container' >
                 <span className='text'>
                     {date.toLocaleTimeString(locale)}
                 </span>
                 <br/>
-                <span className='text'>
-                    {containerAll.length}
-                </span>
-                <br/>
-                <button type='button' onClick={()=>this.handleClick('en-US')}>CLick Here</button>
+                {locale === this.props.locale ? (
+                    <Button change={this.handleClick} locale='en-US' show enable={false}/>
+                ):(
+                    <Button change={this.handleClick} locale={this.props.locale} show={false} enable/>
+                )}
+
             </h1>
         );
     }
