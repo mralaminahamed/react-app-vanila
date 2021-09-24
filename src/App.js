f94c30448b7e3f33cd8d49e56e4d54b505b9f85e
আল আমিন আহম্মেদ <mrabir.ahamed@gmail.com>
2021-09-25 00:47:13 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import Clock from './Components/Clock';
-import Images from './Components/Images';
-import Increment from './Components/Increment';
-import Toggle from './Components/Toggle';
+// import Clock from './Components/Clock';
+//import Images from './Components/Images';
+// import Increment from './Components/Increment';
+// import Toggle from './Components/Toggle';
+//import ClockList from "./Components/ClockList";
+import Form from "./Components/Form";
 
 function App() {
+  //let quantities = [1, 2, 3];
   return [
-    <Clock locale='bn-BD' />,
-    <Clock locale='en' />,
-    <Clock locale='ar-EG' />,
-    <Increment />,
-    <Toggle />,
-    <Toggle />,
-    <Images />,
+      <Form key={1}/>
+    //<ClockList quantities={quantities} />,
+    // <Clock locale='bn-BD' key={Math.random()} />,
+    // <Clock locale='en' key={Math.random()}/>,
+    // <Clock locale='ar-EG' key={Math.random()} />,
+    // <Increment key={Math.random()}  />,
+    // <Toggle key={Math.random()} />,
+    // <Toggle key={Math.random()} />,
+    // <Images />,
   ];
 }
 
