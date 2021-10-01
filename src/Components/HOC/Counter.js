46bdad29fa575a4bf0660991855c16f7409b1ce9
Al-Amin Ahamed <mrabir.ahamed@gmail.com>
2021-10-01 10:32:39 +0600

    Create Counter.js

---
+++ src/Components/HOC/Counter.js
@@ -0,0 +1,17 @@
+import {Component} from "react";
+
+export default class Counter extends Component {
+    state = { count: 0 };
+
+    incrementCount = () => {
+        this.setState((prevState) => (
+            {count: prevState.count + 1}
+        ));
+    }
+
+    render() {
+        const {count} = this.state;
+        const {children} = this.props;
+        return children(count,this.incrementCount);
+    }
+}
