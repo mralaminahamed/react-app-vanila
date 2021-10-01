1537f709d7a592eb84abcd035f5e747e60443493
Al-Amin Ahamed <mrabir.ahamed@gmail.com>
2021-10-01 10:32:57 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,57 @@
 // import Increment from './Components/Increment';
 // import Toggle from './Components/Toggle';
 //import ClockList from "./Components/ClockList";
-import Form from "./Components/Form";
+//import Form from "./Components/Form";
+//import Emoji from "./Components/composetion/Emoji";
+//import Text from "./Components/composetion/Text";
+//import Bracket from "./Components/composetion/Brackets";
+//import ClickCounter from "./Components/HOC/ClickCounter";
+//import HoverCounter from "./Components/HOC/HoverCounter";
 
-function App() {
-  //let quantities = [1, 2, 3];
-  return [
-      <Form key={1}/>
+//import Counter from "./Components/HOC/Counter";
+//import ClickCounter from "./Components/HOC/ClickCounter";
+//import HoverCounter from "./Components/HOC/HoverCounter";
+
+import Section from "./Components/Template/Section";
+import {Component} from "react";
+import ThemeContext from './contexts/themeContext'
+
+export default class App extends Component {
+    state = {
+        theme: 'dark'
+    }
+
+    switchTheme = () => {
+        this.setState(({theme}) => {
+            if (theme === 'dark') {
+                return {theme: 'light'}
+            }
+            return {theme: 'dark'}
+        })
+    }
+
+    render() {
+        const {theme} = this.state;
+        return (
+            <div className='container'>
+                <ThemeContext.Provider value={{theme, switchTheme : this.switchTheme}}>
+                    <Section/>
+                </ThemeContext.Provider>
+            </div>
+        )
+    }
+
+    //let quantities = [1, 2, 3];
+    // return (
+    //     <Emoji>
+    //       {({addEmoji}) => (
+    //           <Bracket>
+    //             {({addBracket}) => <Text addEmoji={addEmoji} addBracket={addBracket}/>}
+    //           </Bracket>
+    //       )}
+    //     </Emoji>
+    // );
+    //<Form key={1}/>
     //<ClockList quantities={quantities} />,
     // <Clock locale='bn-BD' key={Math.random()} />,
     // <Clock locale='en' key={Math.random()}/>,
@@ -17,7 +62,5 @@ function App() {
     // <Toggle key={Math.random()} />,
     // <Toggle key={Math.random()} />,
     // <Images />,
-  ];
-}
 
-export default App;
+}
