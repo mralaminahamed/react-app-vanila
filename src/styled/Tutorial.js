bbe4326e948aab495b39c1868d593f6226395c08
Al-Amin Ahamed <mrabir.ahamed@gmail.com>
2021-10-04 06:10:10 +0600

    Create Tutorial.js

---
+++ src/styled/Tutorial.js
@@ -0,0 +1,10 @@
+import {Container} from "./Components.styles";
+import Card from "./Card";
+
+export default function Tutorial (){
+    return (
+        <Container>
+            <Card/>
+        </Container>
+    )
+}
