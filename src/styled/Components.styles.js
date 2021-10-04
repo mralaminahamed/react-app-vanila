1ecaff2db24f1205bbd8a2f7426a7c8fb2f06273
Al-Amin Ahamed <mrabir.ahamed@gmail.com>
2021-10-04 06:10:20 +0600

    Create Components.styles.js

---
+++ src/styled/Components.styles.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+
+export const Components = styled.section`
+  //background: black;
+  padding-top: 100px;
+  padding-bottom: 100px;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: 'Montserrat', sans-serif;
+  //background: #262626;
+  width: 100%;
+  height: 100vh;
+`;
+
+export const Card = styled.div`
+    width: 100%;
+  color: #5aa9c4;
+`;
+
+
